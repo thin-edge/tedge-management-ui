46dc4e076ba1727250b86279455109704bc6bf2a
Reuben Miller <reuben.d.miller@gmail.com>
2024-01-10 22:19:47 +0100

    feat: graceful shutdown of mqtt-collector

--- mqtt-collector/__main__.py
+++ mqtt-collector/__main__.py
@@ -1,17 +1,24 @@
+import signal
+import sys
 from mongo import Mongo
 from mqtt import MQTT
-from signal import pause
+
+def terminate(_signal, _frame):
+    print("Stopping...", file=sys.stderr)
+    sys.exit(0)
 
 mongo = Mongo()
 mqtt = MQTT(mongo)
+signal.signal(signal.SIGTERM, terminate)
 
 mongo.connect()
 mqtt.run()
 
 try:
-    pause()
+    signal.pause()
 except KeyboardInterrupt:
     pass
-
-mqtt.stop()
-mongo.disconnect()
+finally:
+    print("Disconnecting from mqtt and mongodb...", file=sys.stderr)
+    mqtt.stop()
+    mongo.disconnect()

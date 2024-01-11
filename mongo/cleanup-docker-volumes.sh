4a66c593d30acfbb2cd76c84a860571c78b051b0
sagIoTPower <christof.strack@softwareag.com>
2024-01-11 18:16:11 +0100

    adapt backend to new structure te top structure

--- mongo/cleanup-docker-volumes.sh
+++ mongo/cleanup-docker-volumes.sh
@@ -1,5 +1,5 @@
 # remove created mongo volumes
-docker volume rm management-ui_mongoconfig
-docker volume rm management-ui_mongodata1
-docker volume rm management-ui_mongodata2
-docker volume rm management-ui_nodereddata
+docker volume rm tedge-management-ui_mongoconfig
+docker volume rm tedge-management-ui_mongodata1
+docker volume rm tedge-management-ui_mongodata2
+docker volume rm tedge-management-ui_nodereddata

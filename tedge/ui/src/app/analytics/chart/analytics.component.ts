c9520c7779fb9b3e7ce678521d2915464fbde515
sagIoTPower <christof.strack@softwareag.com>
2024-01-12 18:09:15 +0100

    charts now store selected measurements types

--- tedge/ui/src/app/analytics/chart/analytics.component.ts
+++ tedge/ui/src/app/analytics/chart/analytics.component.ts
@@ -1,85 +1,80 @@
-
-
-import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EdgeService } from '../../edge.service';
 import { RawListItem, SpanListItem } from '../../property.model';
 import { unitList, spanList } from './widget-helper';
 
 @Component({
-  selector: 'app-analytics',
+  selector: 'tedge-analytics',
   templateUrl: './analytics.component.html',
   styleUrls: ['./analytics.component.less']
 })
-export class AnalyticsComponent implements OnInit, OnDestroy {
-
+export class AnalyticsComponent implements OnInit {
   public showDialog: boolean = false;
   public onChangeConfig: EventEmitter<any> = new EventEmitter();
 
-  unitList: RawListItem[] = unitList
+  unitList: RawListItem[] = unitList;
   spanList: SpanListItem[] = spanList;
   config: any = {
-    diagramName: 'Analytics'
-  }
-  rangeUnit: number =  1;
-  rangeUnitCount : number = 2;  // defaults to 5 minutes
+    diagramName: 'Analytics',
+    selectedMeasurements: []
+  };
+  rangeUnit: number = 1;
+  rangeUnitCount: number = 2; // defaults to 5 minutes
   displaySpanIndex: number = 0;
   dateFrom: Date;
   dateTo: Date;
-  bsConfig = {containerClass: "theme-orange", dateInputFormat: 'DD-MM-YYYY'};
+  bsConfig = { containerClass: 'theme-orange', dateInputFormat: 'DD-MM-YYYY' };
   showMeridian = false;
   showSpinners = false;
   type: string;
 
   constructor(
     private edgeService: EdgeService,
-    private router: Router )  { }
-    
+    private router: Router
+  ) {}
+
   async ngOnInit() {
-    let c = await this.edgeService.getAnalyticsConfiguration()
-    console.log("Loaded configuration :", c)
+    const configuration = await this.edgeService.getAnalyticsConfiguration();
+    console.log('Loaded configuration :', configuration);
     this.config = {
       ...this.config,
-      ...c
-    }
+      ...configuration
+    };
 
-    //this.router.url == "/analytics/realtime"
-    let sp = this.router.url.split("/");
-    this.type = sp[sp.length-1];
-    console.log("Chart type:", this.type);
-    if (this.type == "realtime"){
+    // this.router.url == "/analytics/realtime"
+    const sp = this.router.url.split('/');
+    this.type = sp[sp.length - 1];
+    console.log('Chart type:', this.type);
+    if (this.type == 'realtime') {
       this.displaySpanIndex = 0;
     } else {
       this.displaySpanIndex = 1;
     }
 
-    this.dateTo= new Date();
+    this.dateTo = new Date();
     this.dateFrom = new Date();
     this.dateFrom.setMinutes(this.dateFrom.getMinutes() - 5);
   }
 
   configurationChanged(event) {
-    console.log("Configuration changed:", event)
-    this.edgeService.setAnalyticsConfiguration(event).then( c => {
-      this.config = c
-      console.log("Configuration was saved:", c )
-      
-    })
+    console.log('Configuration changed:', event);
+    this.edgeService.setAnalyticsConfiguration(event).then((c) => {
+      this.config = c;
+      console.log('Configuration was saved:', c);
+    });
     this.showDialog = false;
   }
 
   updateFrom() {
-    console.log("Date from:",this.dateFrom)
+    console.log('Date from:', this.dateFrom);
   }
   updateTo() {
-    console.log("Date to:",this.dateTo)
+    console.log('Date to:', this.dateTo);
   }
 
-  updateRangeUnitCount(event)
-  {
-    console.log("RangeUnitCount:",event.target.value)
-    this.rangeUnitCount = event.target.value
+  updateRangeUnitCount(event) {
+    console.log('RangeUnitCount:', event.target.value);
+    this.rangeUnitCount = event.target.value;
   }
-
-  ngOnDestroy(): void {  }
 }
--- tedge/ui/src/app/analytics/chart/charting-config.component.ts
+++ tedge/ui/src/app/analytics/chart/charting-config.component.ts
@@ -1,88 +1,91 @@
-import { Component, OnInit, EventEmitter, Output, Input } from "@angular/core";
-import { EdgeService } from "../../edge.service";
-import { MeasurementType } from "../../property.model";
-import { FormGroup } from "@angular/forms";
-import { FormlyFieldConfig } from "@ngx-formly/core";
-import { BehaviorSubject, Observable, from } from "rxjs";
-import { map, mergeMap, toArray } from "rxjs/operators";
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { EdgeService } from '../../edge.service';
+import { AnalyticsConfiguration, MeasurementType } from '../../property.model';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { isSerieSelected } from '../../share/utils';
 
 @Component({
-  selector: "charting-config",
-  templateUrl: "./charting-config.component.html",
-  styleUrls: ["./charting-config.component.less"],
+  selector: 'tedge-charting-config',
+  templateUrl: './charting-config.component.html',
+  styleUrls: ['./charting-config.component.less']
 })
 export class ChartingConfigComponent implements OnInit {
-  constructor(public edgeService: EdgeService) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    public edgeService: EdgeService
+  ) {}
 
   @Output() onChangeConfig = new EventEmitter<any>();
   @Output() onClose = new EventEmitter<any>();
-  @Input() config: {
-    fillCurve: boolean;
-    fitAxis: boolean;
-    rangeLow: any;
-    rangeHigh: any;
-    diagramName: string;
-  };
+  @Input() config: AnalyticsConfiguration;
   measurementTypes$: Observable<MeasurementType[]>;
   isHidden: boolean = false;
   Object = Object;
 
-  form = new FormGroup({});
+  configForm = new FormGroup({});
+  seriesForm: FormGroup;
+
   fields: FormlyFieldConfig[] = [
     {
-      key: "diagramName",
-      type: "input",
-      wrappers: ["c8y-form-field"],
+      key: 'diagramName',
+      type: 'input',
+      wrappers: ['c8y-form-field'],
       templateOptions: {
-        label: "Digram Name",
-        required: true,
-      },
+        label: 'Digram Name',
+        required: true
+      }
     },
     {
-      key: "fitAxis",
-      type: "checkbox",
-      wrappers: ["c8y-form-field"],
+      key: 'fitAxis',
+      type: 'checkbox',
+      wrappers: ['c8y-form-field'],
       templateOptions: {
-        label: "Fit Axis",
+        label: 'Fit Axis',
         readonly: false,
-        change: (field, $event) => {
+        change: ( field, $event) => {
           this.updateFitAxis();
-        },
-      },
+        }
+      }
     },
     {
-      key: "fillCurve",
-      type: "checkbox",
-      wrappers: ["c8y-form-field"],
+      key: 'fillCurve',
+      type: 'checkbox',
+      wrappers: ['c8y-form-field'],
       templateOptions: {
-        label: "Fill Curve",
-        readonly: false,
-      },
+        label: 'Fill Curve',
+        readonly: false
+      }
     },
     {
-      key: "rangeLow",
-      type: "input",
-      wrappers: ["c8y-form-field"],
-      hideExpression: "model.fitAxis",
+      key: 'rangeLow',
+      type: 'input',
+      wrappers: ['c8y-form-field'],
+      hideExpression: 'model.fitAxis',
       templateOptions: {
-        label: "Lower range y-axis",
-        type: "number",
-        readonly: false,
-      },
+        label: 'Lower range y-axis',
+        type: 'number',
+        readonly: false
+      }
     },
     {
-      key: "rangeHigh",
-      type: "input",
-      wrappers: ["c8y-form-field"],
-      hideExpression: "model.fitAxis",
+      key: 'rangeHigh',
+      type: 'input',
+      wrappers: ['c8y-form-field'],
+      hideExpression: 'model.fitAxis',
       templateOptions: {
-        label: "Higher range y-axis",
-        type: "number",
-        readonly: false,
-      },
-    },
+        label: 'Higher range y-axis',
+        type: 'number',
+        readonly: false
+      }
+    }
   ];
-  async ngOnInit() {
+  ngOnInit() {
+    this.seriesForm = this.formBuilder.group({
+      seriesArray: new FormArray([])
+    });
     this.measurementTypes$ = from(this.edgeService.getMeasurementTypes()).pipe(
       map((mTypes) => {
         const result0 = mTypes.map((mType) => {
@@ -90,31 +93,76 @@ export class ChartingConfigComponent implements OnInit {
             type: mType.type,
             device: mType.device,
             series: mType.series.map((serie) => {
-              return { name: serie, checked: false };
-            }),
+              return {
+                name: serie,
+                selected: isSerieSelected(
+                  mType.device,
+                  mType.type,
+                  serie,
+                  this.config?.selectedMeasurements
+                )
+              };
+            })
           };
           return transformed;
         });
         return result0;
       })
     );
-    console.log("Init: config:", this.config);
+    console.log('Init: config:', this.config);
   }
 
-  public onSaveClicked(): void {
+  onSaveClicked(): void {
     this.onChangeConfig.emit(this.config);
   }
 
-  public onCloseClicked(): void {
+  onCloseClicked(): void {
     this.onClose.emit();
   }
 
-  public updateFitAxis() {
-    console.log("Adapting fit, before:", this.config);
+  updateFitAxis() {
+    console.log('Adapting fit, before:', this.config);
     if (this.config.fitAxis) {
       delete this.config.rangeLow;
       delete this.config.rangeHigh;
     }
-    console.log("Adapting fit, after:", this.config);
+    console.log('Adapting fit, after:', this.config);
+  }
+
+  selectSerie(event) {
+    // mType.device + '___' + mType.type + '___' + serie.name
+    const [device, type, name] = event.target.id.split('___');
+    console.log('Selected serie', event);
+    const {selectedMeasurements} = this.config;
+    const mtss = selectedMeasurements.filter(
+      (mt) => mt.device == device && mt.type == type
+    );
+    let mts;
+    if (!mtss || mtss.length == 0) {
+      mts = {
+        type,
+        device,
+        series: [{ selected: event.target.checked, name }]
+      };
+      selectedMeasurements.push(mts);
+    } else {
+      // update existing measurementType
+      mts = mtss[0];
+      // find relevant series
+      if (!mts.series) {
+        mts.series = [];
+      }
+      const sers = mts.series.filter((ser) => ser.name == name);
+      let ser;
+      if (!sers || sers.length == 0) {
+        ser = {
+          name,
+          selected: event.target.checked
+        };
+        mts.series.push(ser);
+      } else {
+        sers[0].selected = event.target.checked;
+      }
+    }
   }
 }

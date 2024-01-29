b3f366fe4b087c514594e9865bd7f98986e654db
sagIoTPower <christof.strack@softwareag.com>
2024-01-29 10:19:34 +0100

    added fversion to aggregat enumber of stored measurement types

--- tedge/server/app/tedgeFileStore.js
+++ tedge/server/app/tedgeFileStore.js
@@ -62,8 +62,9 @@ class TedgeFileStore {
         TedgeFileStore.childLogger.debug(
           `Found seriesStored: ${JSON.stringify(self.seriesStored)}`
         );
+        const result = this.aggregateAttributes(self.seriesStored);
         TedgeFileStore.childLogger.info(
-            `Found seriesStored: ${JSON.stringify(self.seriesStored)}`
+            `Found seriesStored: ${JSON.stringify(result)}`
           );
         let selfAgain = self;
         setInterval(async function () {
@@ -182,6 +183,28 @@ class TedgeFileStore {
         err
       );
     }
+  }
+
+  aggregateAttributes(obj, level = 0) {
+    const count = {};
+  
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object') {
+          count[key] = this.aggregateAttributes(obj[key], level + 1);
+        } else {
+          count[key] = 1;
+        }
+      }
+    }
+  
+    // Sum the counts of child attributes at the current level
+    const childCount = Object.values(count).reduce((acc, val) => acc + (typeof val !== 'object'? val: 1), 0);
+  
+    return {
+      attributes: childCount,
+      children: count
+    };
   }
 
   static async fileExists(filename) {

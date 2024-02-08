c0c19850f5a6acc509d103805417088f3facf2e5
sagIoTPower <christof.strack@softwareag.com>
2024-02-08 11:57:30 +0100

    test migration

--- tedge/ui/src/bootstrap.ts
+++ tedge/ui/src/bootstrap.ts
@@ -1,18 +1,18 @@
 import './polyfills';
-import './i18n';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
-if (environment.production) {
+import './locales/de.po';
+
+declare const __MODE__: string;
+if (__MODE__ === 'production') {
   enableProdMode();
 }
 
 export function bootstrap() {
   return platformBrowserDynamic()
     .bootstrapModule(AppModule)
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 }

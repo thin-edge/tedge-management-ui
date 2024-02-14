1399eb800c468e6c50d6f498a3269f6c667fb475
sagIoTPower <christof.strack@softwareag.com>
2024-02-14 13:34:05 +0100

    add parameter for mongo port mongo host

--- mongo/mongo-setup.js
+++ mongo/mongo-setup.js
@@ -1,26 +1,31 @@
+
+mongo_port = process.env['MONGO_PORT'];
+mongo_host = process.env['MONGO_HOST'];
+
 rsconf = {
-  _id: "rsmongo",
+  _id: 'rsmongo',
   members: [
     {
       _id: 0,
-      host: "mongodb1:27017",
+      host: `${mongo_host}:${mongo_port}`,
       priority: 1,
     },
   ],
 };
-
 rs.initiate(rsconf);
-let st = rs.status();
-print("Waiting (extra) for replication set creation, status rs:", st["ok"]);
+print(`Creating replication set: ${mongo_host}:${mongo_port}`);
 sleep(5000);
+let st = rs.status();
+print('Waiting (extra) for replication set creation, status rs:', st['ok']);
 
 // create collections and index with ttl, so old measurements are deleted automatically
 keys = { datetime: 1 };
-ttl = process.env["TTL_DOCUMENT"];
+ttl = process.env['TTL_DOCUMENT'];
 options = {
   expireAfterSeconds: parseInt(ttl),
-  name: "datetime_ttl",
+  name: 'datetime_ttl',
 };
-print("Setting TTL for measurements to:", ttl);
+print('Setting TTL for measurements to:', ttl);
+db.createCollection('measurement');
 db.measurement.createIndex(keys, options);
-db.createCollection("serie");
+db.createCollection('serie');
